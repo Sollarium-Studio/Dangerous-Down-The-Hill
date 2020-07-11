b5582ed8c35711ed2eaee49ff7d562e5a220fbd7
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2020-07-11 19:44:23 -0300

    Protótipo menu

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public bool gameCredits;
+    public bool gameMenu;
+    public GameObject panelCredits;
+    public GameObject panelMenu;
+    //public GameObject creditsButton;
+
+    public void inCredits()
+    {
+        if(gameCredits == false)
+        {
+            gameCredits = true;
+            gameMenu = false;
+            panelCredits.SetActive(true);
+            panelMenu.SetActive(false);
+        }
+        else
+        {
+            gameCredits = false;
+            gameMenu = true;
+            panelCredits.SetActive(false);
+            panelMenu.SetActive(true);
+        }
+    }
+
+    public void backMenu()
+    {
+        inCredits();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+         
+}

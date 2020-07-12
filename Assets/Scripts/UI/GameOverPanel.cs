cad6b5337063d43c8b986de83a7360578dcd53b4
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2020-07-12 13:53:18 -0300

    Correção de UI

--- Assets/Scripts/UI/GameOverPanel.cs
+++ Assets/Scripts/UI/GameOverPanel.cs
@@ -13,13 +13,16 @@ public class GameOverPanel : MonoBehaviour
         else Destroy(gameObject);
     }
 
-    [SerializeField] private TextMeshProUGUI scoreText;
-    [SerializeField] private TMP_InputField nickInputField;
+    [SerializeField] private Text scoreText;
+    //[SerializeField] private TMP_InputField nickInputField;
     [SerializeField] private Button backMenu;
+    [SerializeField] private Button playAgain;
 
     private void Start()
     {
         backMenu.onClick.AddListener(() => { SceneManager.LoadScene("Menu"); });
+        playAgain.onClick.AddListener(() => { SceneManager.LoadScene("SampleScene"); });
+
 
         scoreText.text = $"Score: {GameController.instance.GetScore():N0}";
     }
